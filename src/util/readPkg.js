5573dfdb8e12ee80d3c8cf5644513b8cb445c15b
Evan Plaice <evanplaice@gmail.com>
2019-12-15 18:58:13 -0700

    Refactor readPkg()

--- src/util/readPkg.js
+++ src/util/readPkg.js
@@ -5,12 +5,12 @@ const readFileAsync = promisify(readFile);
 
 export async function readPkg (path) {
   if (!await fileStatAsync(path)) {
-    throw Error('ERR_CONFIG: package.json not found, is this a package?');
+    throw Error('package.json not found, is this a package?');
   }
 
   try {
     return JSON.parse(await readFileAsync(path, 'utf-8'));
   } catch {
-    throw Error('ERR_CONFIG: Failed to read package.json');
+    throw Error('Failed to read package.json');
   }
 }
