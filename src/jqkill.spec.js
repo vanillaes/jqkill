5a0b7b0898c3facbb9655cd300166d0e6dafac25
Evan Plaice <evanplaice@gmail.com>
2019-12-16 18:53:25 -0700

    SemiColon Test Passes

--- src/jqkill.spec.js
+++ src/jqkill.spec.js
@@ -7,17 +7,30 @@ const consoleError = console.error;
 let errorOutput = [];
 
 const basic = readFixture('basic.js');
+const semiColon = readFixture('semi-colon.js');
 
 test('setup', (t) => {
   console.error = e => { errorOutput.push(e); };
   t.end();
 });
 
-test('BasicUsage', async (t) => {
+test('Basic - Should work with a basic jQuery selector', async (t) => {
   const expect = [
     '0:0: $(\'#test\')'
   ];
   jqkill(basic);
+
+  t.deepEqual(errorOutput, expect);
+
+  errorOutput = [];
+  t.end();
+});
+
+test('SemiColon - Should work with a semicolon at the end', async (t) => {
+  const expect = [
+    '0:0: $(\'#test\')'
+  ];
+  jqkill(semiColon);
 
   t.deepEqual(errorOutput, expect);
 
