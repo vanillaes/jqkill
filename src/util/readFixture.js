7a91030760ed35aef0878519a50dda14018bb6a4
Evan Plaice <evanplaice@gmail.com>
2019-12-16 18:34:09 -0700

    Fix readFixture()

--- src/util/readFixture.js
+++ src/util/readFixture.js
@@ -1,17 +1,6 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-import { stat, readFile } from 'fs';
-import { promisify } from 'util';
-const fileStatAsync = promisify(stat);
-const readFileAsync = promisify(readFile);
-
-export async function readFixture (path) {
-  if (!await fileStatAsync(path)) {
-    throw Error(`${path} not found, is this a package?`);
-  }
-
-  try {
-    return JSON.parse(await readFileAsync(path, 'utf-8'));
-  } catch {
-    throw Error(`Failed to read ${path}`);
-  }
+export function readFixture (path) {
+  return readFileSync(join(process.cwd(), 'src', '__test__', path), 'utf-8');
 }
