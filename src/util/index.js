3a9015301a321658cf12010b52d64bfdcd097a76
Evan Plaice <evanplaice@gmail.com>
2019-12-18 23:22:57 -0700

    Add basePath Constant

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,5 @@
 export { eachLimit } from './eachLimit.js';
+export { basePath } from './basePath.js';
 export { match } from './match.js';
 export { readContents } from './readContents.js';
 export { readFixture } from './readFixture.js';
